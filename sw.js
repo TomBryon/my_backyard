d853d5c6f8e5ecd5df6787c82cd9c960b7ef82e2
Alharbi103 <64801834+Alharbi103@users.noreply.github.com>
2020-05-15 01:25:34 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -15,6 +15,18 @@ self.addEventListener('install', function(event) {
 				"./style.css",
 				"./manifest.json",
 				"./jquery-3.4.1.js"
+				
+				//those resources are cached for Reminder
+				"/jquery-3.4.1.js",
+				"/reminders/reminders.html",
+				"/reminders/reminders.js",
+				"/css/themes/Try1.css",
+				"/css/themes/jquery.mobile.icons.min.css",
+				"/jquery.mobile-1.4.5.min.js",
+				"/jquery-1.11.1.min.js",
+				"/jquery.mobile.structure-1.4.5.min.css",
+				"/jquery.mobile-1.4.5.min.map",
+				"/css/themes/images/ajax-loader.gif"
 			]);
 		})
 	);
@@ -62,3 +74,25 @@ self.addEventListener('fetch', function(event) {
 	}
 
 });
+
+// i do not if i should change it or keep it?
+
+// self.addEventListener('activate', function(event) {
+// 	// Delete old asset caches.
+// 	event.waitUntil(
+// 		self.clients.claim()
+// 	);
+// });
+
+// //it is a middleware, when browser fetches resources, it always happens via this function
+// self.addEventListener('fetch', function(event) {
+// 		event.respondWith(
+// 			caches.match(event.request).then(function(response) {
+// 				return response || fetch(event.request)
+// 					.catch(er => {
+// 						console.log(er)
+// 					});
+// 			})
+// 		);
+// });
+
