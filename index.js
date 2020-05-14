2ce242993d131327deb1470a9c88cab0db2cc402
Alharbi103 <64801834+Alharbi103@users.noreply.github.com>
2020-05-15 01:14:47 +1000

    Update index.js
    - register service worker
    -request permission in browser

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 
+
+
+//register service worker
+if ('serviceWorker' in navigator) {
+
+  navigator.serviceWorker
+      .register('/sw.js')
+      .then(function (swReg) {
+        console.log('Service Worker is registered', swReg);
+      })
+      .catch(function (error) {
+        console.error('Service Worker Error', error);
+      });
+}
+
+//it request permission in browser
+if (Notification.requestPermission(result => {
+
+})){}
+
 $(document).ready(function(){
   $("img").click(function(){
     $(this).hide();
